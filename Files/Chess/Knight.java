4b9325ecb6b0d75f1c2529d4b017cc1dce4d766c
PavlovTheDog <coleblax@gmail.com>
2017-05-23 12:39:12 -0400

    Update Knight.java

--- Files/Chess/Knight.java
+++ Files/Chess/Knight.java
@@ -1,57 +1,29 @@
 //John Link Cole Blackman 5/10/17
-
-public class Knight extends Piece 
-{
-	  public static boolean isValidMoveBlack(int fx, int fy, int sx, int sy)
-	  {
-		  if(GUIPanel.b1[fy][fx+1].getText().trim().isEmpty() && GUIPanel.b1[fy][fx-1].getText().trim().isEmpty() && GUIPanel.b1[fy+1][fx].getText().trim().isEmpty() && GUIPanel.b1[fy-1][fx].getText().trim().isEmpty())//if the move is valid for a black pawn (it is empty)
-		  {
-			  if(GUIPanel.b1[sy][sx].getText().trim().isEmpty())
-			  {
-				  if(GUIPanel.isWhiteTurn == false)
-				  {
-				  return true;
-				  }
-				  else
-				  {
-					  return false;
-				  }
-			  }
-			  else
-			  {
-				  return false;
-			  }
-		  }
-		  else
-		  {
-			  return false;
-		  }
-	  }
-		 public static boolean isValidMoveWhite(int fx2, int fy2, int sx2, int sy2)
-		  {
-			  if(GUIPanel.b1[fy2][fx2+1].getText().trim().isEmpty() && GUIPanel.b1[fy2][fx2-1].getText().trim().isEmpty() && GUIPanel.b1[fy2+1][fx2].getText().trim().isEmpty() && GUIPanel.b1[fy2-1][fx2].getText().trim().isEmpty())//if the move is valid for a black pawn (it is empty)
-			  {
-				  if(GUIPanel.b1[sy2][sx2].getText().trim().isEmpty())
-				  {
-					  if(GUIPanel.isWhiteTurn == true)
-					  {
-					  return true;
-					  }
-					  else
-					  {
-						  return false;
-					  }
-				  }
-				  else
-				  {
-					  return false;
-				  }
-			  }
-			  else
-			  {
-				  return false;
-			  }
-		  }
-	  
-	  
+public class Knight extends Piece {
+	public static boolean isValidMoveBlack(int fy, int fx, int sx, int sy)
+	{
+		if(sy >= 9 || sx >= 9)
+		{
+			return false;
+			
+		}
+			
+		else if(GUIPanel.b1[sx][sy].getText() != "")
+		{
+			return false;
+		}
+		else if(   ((fx + 1 == sx) && (fy + 2 == sy || fy - 2 == sy)) 
+				|| ((fx + 2 == sx) && (fy + 1 == sy || fy - 1 == sy)) 
+				|| ((fx - 1 == sx) && (fy + 2 == sy || fy - 2 == sy)) 
+				|| ((fx - 2 == sx) && (fy + 1 == sy || fy - 1 == sy)))
+		{
+			
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+		
+	}
 }
