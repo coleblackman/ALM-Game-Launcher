aa79c9aed82c16a18f3bca5e52dad9cd9f48027e
PavlovTheDog <coleblax@gmail.com>
2017-06-01 12:59:32 -0400

    Update King.java

--- Files/Chess/King.java
+++ Files/Chess/King.java
@@ -1,11 +1,34 @@
 //John Link Cole Blackman 5/10/17
+
 import java.awt.Color;
 public class King extends Piece {
 		
-	  public static boolean isValidMoveBlack(int fy, int fx, int sx, int sy)
+	  public static boolean isValidMoveBlack(int fx, int fy, int sx, int sy)
 	  {
-		return false;
+		  System.out.println("fx" + fx + "fy" + fy + "sx" + sx + "sy" + sy);
+		  if(GUIPanel.b1[fy][fx].getText() != "\u265A")
+			{
+				return false;
+			}
+		for (int i = 0; i < 6; i++) { //for loop to iterate through all possible white pieces and makes sure that the second position is not a white piece.
+			System.out.println(GUIPanel.b1[sy][sx].getText() + " " + Character.toString(GUIPanel.AllWhitePieces[i]));
+			if (GUIPanel.b1[sy][sx].getText().equals(Character.toString(GUIPanel.AllWhitePieces[i])))
+				return false;
+		}
+		  
+		  
+		  if(Math.abs(sx - fx) != 1 && Math.abs(sy - fy) != 1)
+			  return false;
+		  
+		  
+		  
+		  GUIPanel.WhiteKingHasMoved = true;
+		  return true;
+		  
+		
+		  
 	  }
+	  //Method for determining if this move is valid for a white King
 	  public static boolean isValidMoveWhite(int fx, int fy, int sx, int sy)
 	  {
 		  if(GUIPanel.b1[fy][fx].getText() != "\u2654")
@@ -33,12 +56,13 @@ public class King extends Piece {
 		{
 			
 			GUIPanel.b1[sy][sx].setText("\u265A");
+			GUIPanel.b1[sy][sx].setForeground(Color.BLACK);
 			GUIPanel.b1[fy][fx].setText("");
 			
 		}
 	  public static void wmove(int fx, int fy, int sx, int sy)
 		{
-			
+		  System.out.println("wmove");
 			GUIPanel.b1[sy][sx].setText("\u2654");
 			GUIPanel.b1[sy][sx].setForeground(Color.WHITE);
 			GUIPanel.b1[fy][fx].setText("");
