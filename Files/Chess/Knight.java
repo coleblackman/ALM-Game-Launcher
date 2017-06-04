9d992ca8d2ca08050ddbe4c76ee3b356bd0ca3ee
PavlovTheDog <coleblax@gmail.com>
2017-06-04 10:19:13 -0400

    Update Knight.java

--- Files/Chess/Knight.java
+++ Files/Chess/Knight.java
@@ -1,72 +1,71 @@
+
 //John Link Cole Blackman 5/10/17
 import java.awt.Color;
 
-public class Knight extends Piece {
-	public static boolean isValidMoveBlack(int fy, int fx, int sx, int sy) {
+public class Knight extends Piece
+{
+	public static boolean isValidMoveBlack(int fx, int fy, int sx, int sy)
+	{
+		for (int i = 0; i < 6; i++)
+		{
+
+			if (GUIPanel.b1[sy][sx].getText().equals(Character.toString(GUIPanel.AllBlackPieces[i])))
+				return false;
+		}
 		if (GUIPanel.b1[fy][fx].getText() != "\u265E")
-			return false;
-		if (GUIPanel.b1[sx][sy].getText() != "" || GUIPanel.b1[sx][sy].getForeground() == Color.BLACK) {
+		{
 			return false;
 		}
+		if ((((fx + 1 == sx) || (fx - 1 == sx)) && (fy - 2 == sy))
 
-		if (!(((fy + 1 == sx) && (fx + 2 == sy || fx - 2 == sy)) || ((fy + 2 == sx) && (fx + 1 == sy || fx - 1 == sy))
-				|| ((fy - 1 == sx) && (fx + 2 == sy || fx - 2 == sy))
-				|| ((fy - 2 == sx) && (fx + 1 == sy || fx - 1 == sy)))) {
-
-			return false;
-		} else {
+				|| (((fx + 2 == sx) || (fx - 2 == sx)) && (fy - 1 == sy))
+				|| (((fx + 1 == sx) || (fx - 1 == sx)) && (fy + 2 == sy))
+				|| (((fx + 2 == sx) || (fx - 2 == sx)) && (fy + 1 == sy)))
 			return true;
-		}
+		else
+			return false;
 
 	}
 
-	public static boolean isValidMoveWhite(int fx, int fy, int sx, int sy) {
-		// System.out.println("WOO"+fx+fy+sx+sy + "\u2656" +
-		// GUIPanel.b1[fy][fx].getText());
-		for (int i = 0; i < 6; i++) {
-			System.out.println(GUIPanel.b1[sy][sx].getText() + " " + Character.toString(GUIPanel.AllWhitePieces[i]));
+	public static boolean isValidMoveWhite(int fx, int fy, int sx, int sy)
+	{
+
+		for (int i = 0; i < 6; i++)
+		{
+
 			if (GUIPanel.b1[sy][sx].getText().equals(Character.toString(GUIPanel.AllWhitePieces[i])))
 				return false;
 		}
-		if (GUIPanel.b1[fy][fx].getText() != "\u2658") {
-			System.out.println("Yes");
-			return false;
-		}
-		if (GUIPanel.b1[sx][sy].getText() != "" && GUIPanel.b1[sx][sy].getText() != "\u2658"
-				&& GUIPanel.b1[sx][sy].getText() != "\u2657" && GUIPanel.b1[sx][sy].getText() != "\u2654"
-				&& GUIPanel.b1[sx][sy].getText() != "\u265A" && GUIPanel.b1[sx][sy].getText() != "\u2655"
-				&& GUIPanel.b1[sx][sy].getText() != "\u265B") {
-			// System.out.println("Hello");
+		if (GUIPanel.b1[fy][fx].getText() != "\u2658")
+		{
 			return false;
 		}
 
-		System.out.println("WOO" + fx + fy + sx + sy + "\u2656" + GUIPanel.b1[fy][fx].getText());
-		if (sx != fx - 1 && sx != fx + 1 && sx != fx + 2 && sx != fx - 2 && sy != fy - 2 && sy != fy + 2 && sy != fy - 1
-				&& sy != fy + 1) {
-			System.out.println("made it");
+		if ((((fx + 1 == sx) || (fx - 1 == sx)) && (fy - 2 == sy))
+
+				|| (((fx + 2 == sx) || (fx - 2 == sx)) && (fy - 1 == sy))
+				|| (((fx + 1 == sx) || (fx - 1 == sx)) && (fy + 2 == sy))
+				|| (((fx + 2 == sx) || (fx - 2 == sx)) && (fy + 1 == sy)))
+			return true;
+		else
 			return false;
-
-		}
-
-		return true;
-
 	}
 
-	public static void bmove(int fx, int fy, int sx, int sy) {
+	public static void bmove(int fx, int fy, int sx, int sy)
+	{
 
 		GUIPanel.b1[sy][sx].setText("\u265E");
+		GUIPanel.b1[sy][sx].setForeground(Color.BLACK);
 		GUIPanel.b1[fy][fx].setText("");
-
-		
 
 	}
 
-	public static void wmove(int fx, int fy, int sx, int sy) {
+	public static void wmove(int fx, int fy, int sx, int sy)
+	{
 
 		GUIPanel.b1[sy][sx].setText("\u2658");
 		GUIPanel.b1[sy][sx].setForeground(Color.WHITE);
 		GUIPanel.b1[fy][fx].setText("");
-		System.out.println("wmove");
 
 	}
 }
