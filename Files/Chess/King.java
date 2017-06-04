b391812ec1b68d15dda66e3926e4ce24269feb3e
PavlovTheDog <coleblax@gmail.com>
2017-06-04 10:19:43 -0400

    Update King.java

--- Files/Chess/King.java
+++ Files/Chess/King.java
@@ -1,71 +1,70 @@
-//John Link Cole Blackman 5/10/17
 
+//John Link Cole Blackman 5/10/17
 import java.awt.Color;
-public class King extends Piece {
-		
-	  public static boolean isValidMoveBlack(int fx, int fy, int sx, int sy)
-	  {
-		  System.out.println("fx" + fx + "fy" + fy + "sx" + sx + "sy" + sy);
-		  if(GUIPanel.b1[fy][fx].getText() != "\u265A")
-			{
+
+public class King extends Piece
+{
+
+	public static boolean isValidMoveBlack(int fx, int fy, int sx, int sy)
+	{
+		if (GUIPanel.b1[fy][fx].getText() != "\u265A")
+		{
+			return false;
+		}
+		for (int i = 0; i < 6; i++)
+		{ // for loop to iterate through all
+			// possible white pieces and makes sure
+			// that the second position is not a
+			// white piece.
+			if (GUIPanel.b1[sy][sx].getText().equals(Character.toString(GUIPanel.AllBlackPieces[i])))
 				return false;
-			}
-		for (int i = 0; i < 6; i++) { //for loop to iterate through all possible white pieces and makes sure that the second position is not a white piece.
-			System.out.println(GUIPanel.b1[sy][sx].getText() + " " + Character.toString(GUIPanel.AllWhitePieces[i]));
+		}
+		if (Math.abs(sx - fx) != 1 && Math.abs(sx - fx) != 0)
+			return false;
+		if (Math.abs(sy - fy) != 1 && Math.abs(sy - fy) != 0)
+			return false;
+
+		return true;
+
+	}
+
+	// Method for determining if this move is valid for a white King
+	public static boolean isValidMoveWhite(int fx, int fy, int sx, int sy)
+	{
+		if (GUIPanel.b1[fy][fx].getText() != "\u2654")
+		{
+			return false;
+		}
+		for (int i = 0; i < 6; i++)
+		{ // for loop to iterate through all
+			// possible white pieces and makes sure
+			// that the second position is not a
+			// white piece.
 			if (GUIPanel.b1[sy][sx].getText().equals(Character.toString(GUIPanel.AllWhitePieces[i])))
 				return false;
 		}
-		  
-		  
-		  if(Math.abs(sx - fx) != 1 && Math.abs(sy - fy) != 1)
-			  return false;
-		  
-		  
-		  
-		  GUIPanel.WhiteKingHasMoved = true;
-		  return true;
-		  
-		
-		  
-	  }
-	  //Method for determining if this move is valid for a white King
-	  public static boolean isValidMoveWhite(int fx, int fy, int sx, int sy)
-	  {
-		  if(GUIPanel.b1[fy][fx].getText() != "\u2654")
-			{
-				return false;
-			}
-		for (int i = 0; i < 6; i++) { //for loop to iterate through all possible white pieces and makes sure that the second position is not a white piece.
-			System.out.println(GUIPanel.b1[sy][sx].getText() + " " + Character.toString(GUIPanel.AllWhitePieces[i]));
-			if (GUIPanel.b1[sy][sx].getText().equals(Character.toString(GUIPanel.AllWhitePieces[i])))
-				return false;
-		}
-		  
-		  
-		  if(Math.abs(sx - fx) != 1 && Math.abs(sy - fy) != 1)
-			  return false;
-		  
-		  
-		  
-		  GUIPanel.WhiteKingHasMoved = true;
-		  return true;
-		  
-		
-	  }
-	  public static void bmove(int fx, int fy, int sx, int sy)
-		{
-			
-			GUIPanel.b1[sy][sx].setText("\u265A");
-			GUIPanel.b1[sy][sx].setForeground(Color.BLACK);
-			GUIPanel.b1[fy][fx].setText("");
-			
-		}
-	  public static void wmove(int fx, int fy, int sx, int sy)
-		{
-		  System.out.println("wmove");
-			GUIPanel.b1[sy][sx].setText("\u2654");
-			GUIPanel.b1[sy][sx].setForeground(Color.WHITE);
-			GUIPanel.b1[fy][fx].setText("");
-			
-		}
+		if (Math.abs(sx - fx) != 1 && Math.abs(sx - fx) != 0)
+			return false;
+		if (Math.abs(sy - fy) != 1 && Math.abs(sy - fy) != 0)
+			return false;
+
+		return true;
+	}
+
+	public static void bmove(int fx, int fy, int sx, int sy)
+	{
+
+		GUIPanel.b1[sy][sx].setText("\u265A");
+		GUIPanel.b1[sy][sx].setForeground(Color.BLACK);
+		GUIPanel.b1[fy][fx].setText("");
+
+	}
+
+	public static void wmove(int fx, int fy, int sx, int sy)
+	{
+		GUIPanel.b1[sy][sx].setText("\u2654");
+		GUIPanel.b1[sy][sx].setForeground(Color.WHITE);
+		GUIPanel.b1[fy][fx].setText("");
+
+	}
 }
